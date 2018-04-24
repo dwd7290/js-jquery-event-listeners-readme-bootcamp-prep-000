2edd61815a1ffba2ab3ba1ad86d627be51d53ee0
David <dwd7290@yahoo.com>
2018-04-24 05:10:15 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -12,8 +12,11 @@ function frameIt() {
 }
 
 function pressIt() {
-  
+  $('input').on('keydown', function(key) {
+    if (key.which == )
+  });  
 }
+
 $(document).ready(function(){
 
 // call functions here
